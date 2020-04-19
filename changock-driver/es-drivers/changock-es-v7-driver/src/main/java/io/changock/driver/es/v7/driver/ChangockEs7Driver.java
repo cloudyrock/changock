8f0f2975a370cafc1b33fa5bf70a269e61fb8bc3
dieppa <aperezdieppa@gmail.com>
2020-04-19 16:43:36 +0100

    wip

--- changock-driver/es-drivers/changock-es-v7-driver/src/main/java/io/changock/driver/es/v7/driver/ChangockEs7Driver.java
+++ changock-driver/es-drivers/changock-es-v7-driver/src/main/java/io/changock/driver/es/v7/driver/ChangockEs7Driver.java
@@ -2,16 +2,26 @@ package io.changock.driver.es.v7.driver;
 
 import io.changock.driver.api.entry.ChangeEntry;
 import io.changock.driver.api.entry.ChangeEntryService;
+import io.changock.driver.es.v7.repository.EsChangeEntryRepository;
 import org.elasticsearch.client.Client;
+import io.changock.utils.annotation.NotThreadSafe;
 
+@NotThreadSafe
 public class ChangockEs7Driver extends ChangockEs7DriverBase<ChangeEntry> {
+
+  protected EsChangeEntryRepository<ChangeEntry> changeEntryRepository;
+
 
   public ChangockEs7Driver(Client esClient) {
     super(esClient);
   }
 
+  //TODO implement this properly: constructor arguments, etc.
   @Override
   public ChangeEntryService<ChangeEntry> getChangeEntryService() {
-    return null;
+    if (changeEntryRepository == null) {
+      this.changeEntryRepository = new EsChangeEntryRepository<>();
+    }
+    return changeEntryRepository;
   }
 }
